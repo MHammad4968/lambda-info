c23236d362353864b894af14d2b9dfe2915a447d
MHammad4968 <hammad.mh.4968@gmail.com>
2024-02-07 13:14:35 +0530

    Added a function that prints total and free memory of the machine

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const os = require(os);
+const os = require('os');
+var tmem = Math.floor(os.totalmem()/(1024**2));
+var fmem = Math.floor(os.freemem()/(1024**2));
+
+
+console.log(`Total: ${tmem}MB`);
+console.log(`FreeMem: ${fmem}MB`);
