487dadaf5adf251ad9ebe02e87642fc0fff66158
MHammad4968 <hammad.mh.4968@gmail.com>
2024-02-08 13:00:00 +0530

    Added a basic HTTP Server on port 3000

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const os = require('os');
+const http = require('http');
+const srv = http.createServer((req, res) => {
+    if(req.url === "/") {
+        res.write("Working, epic!");
+        res.end();
+    }
+});
+srv.listen(3000);
 var tmem = Math.floor(os.totalmem()/(1024**2));
 var fmem = Math.floor(os.freemem()/(1024**2));
-
-
-console.log(`Total: ${tmem}MB`);
-console.log(`FreeMem: ${fmem}MB`);
+srv.on("connection", (socket) => {
+    console.log("Connected");
+});
+/*
+console.log(`Total Memory: ${tmem}MB`);
+console.log(`Free Memory: ${fmem}MB`);
 console.log("Platform: "+ os.platform()+"; "+os.type());
 console.log("CPUs: "+ os.cpus().length);
-console.log("Uptime: " + os.uptime());
+console.log("Uptime: " + Math.floor(os.uptime()/60) + "min");
+*/
