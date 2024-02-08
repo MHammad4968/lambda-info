ed557e8aca0ff41be02c7264033414ff4125a1cc
MHammad4968 <hammad.mh.4968@gmail.com>
2024-02-08 13:06:06 +0530

    Moved system info responses to HTTP server and disabled them on console

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
+
 const os = require('os');
+var tmem = Math.floor(os.totalmem()/(1024**2));
+var fmem = Math.floor(os.freemem()/(1024**2));
+
 const http = require('http');
 const srv = http.createServer((req, res) => {
     if(req.url === "/") {
         res.write("Working, epic!");
         res.end();
     }
+    if(req.url === "/spec") {
+        res.write("System specs: \n");
+        res.write(`Total Memory: ${tmem}MB \n`);
+        res.write(`Free Memory: ${fmem}MB\n`);
+        res.write("Platform: "+ os.platform()+"; "+os.type() +"\n");
+        res.write("CPUs: "+ os.cpus().length +"\n");
+        res.write("Uptime: " + Math.floor(os.uptime()/60) + "min");
+        res.end()
+    }
 });
 srv.listen(3000);
-var tmem = Math.floor(os.totalmem()/(1024**2));
-var fmem = Math.floor(os.freemem()/(1024**2));
 srv.on("connection", (socket) => {
     console.log("Connected");
 });
